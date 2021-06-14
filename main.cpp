d7d7cb1507079d1c1d5cc3e78573eee205f1e8e5
Mo Mia <mo@syntaxlabs.co.za>
2021-06-14 17:37:00 +0200

    Re-enabled main program

--- main.cpp
+++ main.cpp
@@ -11,50 +11,49 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // //* Asking level 1 Q's
-    // mcq *p1 = readMCQ("level1.txt");
+    //* Asking level 1 Q's
+    mcq *p1 = readMCQ("level1.txt");
 
-    // for (int i = 1; i < 10; i++)
-    // {
-    //     (p1 + i)->answer = (askMCQ(*(p1 + i), 1));
-    // }
+    for (int i = 1; i < 10; i++)
+    {
+        (p1 + i)->answer = (askMCQ(*(p1 + i), 1));
+    }
 
-    // bool total1 = totalMCQ(p1);
+    bool total1 = totalMCQ(p1);
 
-    // //* Asking level 2 Q's
-    // mcq *p2 = readMCQ("level2.txt");
+    //* Asking level 2 Q's
+    mcq *p2 = readMCQ("level2.txt");
 
-    // for (int i = 1; i < 10; i++)
-    // {
-    //     (p2 + i)->answer = (askMCQ(*(p2 + i), 2));
-    // }
+    for (int i = 1; i < 10; i++)
+    {
+        (p2 + i)->answer = (askMCQ(*(p2 + i), 2));
+    }
 
-    // bool total2 = totalMCQ(p2);
+    bool total2 = totalMCQ(p2);
 
-    // //* Asking level 3 Q's
-    // mcq *p3 = readMCQ("level3.txt");
+    //* Asking level 3 Q's
+    mcq *p3 = readMCQ("level3.txt");
 
-    // for (int i = 1; i < 10; i++)
-    // {
-    //     (p3 + i)->answer = (askMCQ(*(p3 + i), 3));
-    // }
+    for (int i = 1; i < 10; i++)
+    {
+        (p3 + i)->answer = (askMCQ(*(p3 + i), 3));
+    }
 
-    // bool total3 = totalMCQ(p3);
+    bool total3 = totalMCQ(p3);
 
-    // //* Asking level 4 Q's
-    // tfq *p4 = readTFQ("level4.txt");
+    //* Asking level 4 Q's
+    tfq *p4 = readTFQ("level4.txt");
 
-    // for (int i = 1; i < 10; i++)
-    // {
-    //     (p4 + i)->answer = (askTFQ(*(p4 + i), 4));
-    // }
+    for (int i = 1; i < 10; i++)
+    {
+        (p4 + i)->answer = (askTFQ(*(p4 + i), 4));
+    }
 
-    // bool total4 = totalTFQ(p4);
+    bool total4 = totalTFQ(p4);
 
-    // string finalResult = "";
+    string finalResult = "";
 
-    // finalResult = getFinalResult(total1, total2, total3, total4);
-    string finalResult = "0001";
+    finalResult = getFinalResult(total1, total2, total3, total4);
     system("CLS");
 
     displayFinalResult(finalResult);
